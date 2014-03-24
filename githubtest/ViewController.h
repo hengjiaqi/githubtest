b29a1540f905700a1caa159e3948847cf82c7892
hengjiaqi <hengjiaqi@gmail.com>
2014-03-24 12:10:14 -0700

    initial commit

---
+++ githubtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by jake on 3/24/14.
+//  Copyright (c) 2014 jake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ githubtest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  githubtest
+//
+//  Created by jake on 3/24/14.
+//  Copyright (c) 2014 jake. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
